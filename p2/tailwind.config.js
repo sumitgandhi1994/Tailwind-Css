f27aa1cd1da82f4e7eaae8a081f6e69d1b45eb04
sumitgandhi1994 <85626910+sumitgandhi1994@users.noreply.github.com>
2024-11-28 17:34:51 +0000

    Plant Header and banner is ready now

--- p2/tailwind.config.js
+++ p2/tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./dist/*.html'],
+  content: ["./dist/*.html", "./assets/js/*.js"],
   theme: {
+    screens: {
+      sm: "340px",
+      md: "540px",
+      lg: " 768px",
+      xl: "1180px",
+    },
     extend: {},
+    fontFamily: {
+      Jost: ["Jost", "sans-serif"],
+      Lobster: ["Lobster", "sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "12px",
+        md: "32px",
+      },
+    },
   },
   plugins: [],
-}
-
+};
